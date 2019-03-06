a86a8f7806fd58e668b31008ee3693401a6e5442
tlahmann <tobiaslahmann@gmail.com>
2019-03-06 10:44:15 +0100

    Add mouse cursor to the drawing

--- helloColor/helloColor.js
+++ helloColor/helloColor.js
@@ -1,14 +1,20 @@
+'use strict'
+
 function setup () {
     createCanvas(720, 720);
+    noCursor();
 
     colorMode(HSB, 360, 100, 100);
     rectMode(CENTER);
-    noStroke();
 }
 
 function draw () {
-    background(mouseX / 2, 100, 100);
+    background(mouseY / 2, 100, 100);
 
-    fill(360 - mouseX / 2, 100, 100);
-    rect(360, 360, mouseY + 1, mouseY + 1);
+    noStroke();
+    fill(360 - mouseY / 2, 100, 100);
+    rect(360, 360, mouseX + 1, mouseX + 1);
+    stroke(0, 0, 0);
+    line(mouseX, mouseY - 10, mouseX, mouseY + 10);
+    line(mouseX - 10, mouseY, mouseX + 10, mouseY);
 }
