7c64e75916e6a0ea2fe086631115545050f7b018
tlahmann <tobiaslahmann@gmail.com>
2019-03-06 11:29:21 +0100

    Make the canvas use the full size of the body element

--- helloColor/helloColor.js
+++ helloColor/helloColor.js
@@ -1,10 +1,14 @@
 'use strict'
 
+let size
+
 function setup () {
-    createCanvas(720, 720);
+    let b = select('body');
+    size = {width: b.width - 2, height: b.height - 4};
+    createCanvas(size.width, size.height);
     noCursor();
 
-    colorMode(HSB, 360, 100, 100);
+    colorMode(HSB, size.height, 100, 100);
     rectMode(CENTER);
 }
 
@@ -12,8 +16,8 @@ function draw () {
     background(mouseY / 2, 100, 100);
 
     noStroke();
-    fill(360 - mouseY / 2, 100, 100);
-    rect(360, 360, mouseX + 1, mouseX + 1);
+    fill(size.height - mouseY / 2, 100, 100);
+    rect(size.width / 2, size.height / 2, mouseX + 1, mouseX + 1);
     stroke(0, 0, 0);
     line(mouseX, mouseY - 10, mouseX, mouseY + 10);
     line(mouseX - 10, mouseY, mouseX + 10, mouseY);
